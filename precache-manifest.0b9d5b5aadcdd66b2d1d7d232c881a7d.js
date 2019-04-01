941591a56569f8c993f840c8a1dbc3a3af09481a
Sebastian Badon <sebastianmbadon@gmail.com>
2019-03-31 22:58:37 -0400

    Updates

---
+++ precache-manifest.0b9d5b5aadcdd66b2d1d7d232c881a7d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ef2325423da327e2ec",
+    "url": "/React-Hearthstone-Deck-Builder/static/css/main.b699fc16.chunk.css"
+  },
+  {
+    "revision": "a5ef2325423da327e2ec",
+    "url": "/React-Hearthstone-Deck-Builder/static/js/main.8dea9f9b.chunk.js"
+  },
+  {
+    "revision": "487e940d0dd0e1feeb81",
+    "url": "/React-Hearthstone-Deck-Builder/static/js/runtime~main.9818f71b.js"
+  },
+  {
+    "revision": "2bba4e707af302e2a5d4",
+    "url": "/React-Hearthstone-Deck-Builder/static/css/2.0fa8a8f1.chunk.css"
+  },
+  {
+    "revision": "2bba4e707af302e2a5d4",
+    "url": "/React-Hearthstone-Deck-Builder/static/js/2.024962a9.chunk.js"
+  },
+  {
+    "revision": "2ddbf2cf79f4d750d92e2a4bfc01f636",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/druid.2ddbf2cf.png"
+  },
+  {
+    "revision": "28aa72d9924ee719f57b53d577440b5b",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/hunter.28aa72d9.png"
+  },
+  {
+    "revision": "0b00b5016d5a2597cfc9d606967800d3",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/mage.0b00b501.png"
+  },
+  {
+    "revision": "80f26cd425cc66765f1d66b75ef8d4e1",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/paladin.80f26cd4.png"
+  },
+  {
+    "revision": "ac86411b1629866d36f73c2866d4cd1d",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/priest.ac86411b.png"
+  },
+  {
+    "revision": "86ccdfd7b18e753ed994d83b152d83d4",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/rogue.86ccdfd7.png"
+  },
+  {
+    "revision": "c6ec916c1194345c23f8198537258e19",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/shaman.c6ec916c.png"
+  },
+  {
+    "revision": "e6ae4608ee49b040ad1958b5055c71b8",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/warlock.e6ae4608.png"
+  },
+  {
+    "revision": "79f2e2b682640f95dc2eaadf0a48fcda",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/warrior.79f2e2b6.png"
+  },
+  {
+    "revision": "90a6edd8696bc259a572586dba6668d4",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/crystal.90a6edd8.png"
+  },
+  {
+    "revision": "35203464502daad369df695e66e1cfc4",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/wood.35203464.jpg"
+  },
+  {
+    "revision": "c16de4351c61b376743b786eef97f17c",
+    "url": "/React-Hearthstone-Deck-Builder/static/media/dark-honeycomb.c16de435.png"
+  },
+  {
+    "revision": "bfa9157cb0625f2da6a47d37681906c7",
+    "url": "/React-Hearthstone-Deck-Builder/index.html"
+  }
+];
